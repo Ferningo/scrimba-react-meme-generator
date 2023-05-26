27c1713f4926a23d8574195d10f46a034c065a07
Jose <jose.mar.123@hotmail.com>
2023-05-25 19:49:40 -0600

    Add Meme compoenent and initial styles for it

---
+++ meme-generator/src/components/Meme.jsx
@@ -0,0 +1,14 @@
+function Meme(){
+    return(
+        <div className="meme-input-container">
+            <div className="text-input-container">
+                <input type="text" />
+                <input type="text" />
+            </div>
+            <input className="generate-meme-button" type="button" />
+        </div>
+
+    )
+}
+
+export default Meme
