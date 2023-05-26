5992c083d279c139067460fb1e55a3222c3eb182
Jose <jose.mar.123@hotmail.com>
2023-05-25 20:41:03 -0600

    And functions to handle onclik and hover events

--- meme-generator/src/components/Meme.jsx
+++ meme-generator/src/components/Meme.jsx
@@ -1,11 +1,27 @@
 function Meme(){
+    function handleOnClick(){
+        console.log('Click!')
+    }
+
+    function handleOnHover(){
+        console.log('Mouse Hovering!')
+    }
+
+
     return(
         <div className="meme-input-container">
             <div className="text-input-container">
-                <input className="text-input" type="text" />
-                <input type="text" />
+                <input className="text-input" type="text" placeholder="top text" />
+                <input type="text"   placeholder="bottom text"/>
             </div>
-            <button className="generate-meme-button">Get a new meme image!</button>
+            <button 
+                className="generate-meme-button"
+                onClick={handleOnClick}
+                onMouseOver={handleOnHover}
+                >
+                
+                    Get a new meme image! 🖼️
+            </button>
         </div>
 
     )
