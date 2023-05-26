7ae69cdc82d891f1c63265e6e2b9e87008737a4a
Jose <jose.mar.123@hotmail.com>
2023-05-25 19:02:02 -0600

    Add title and logo elements to header

--- meme-generator/src/components/Header.jsx
+++ meme-generator/src/components/Header.jsx
@@ -1,9 +1,13 @@
+import trollFace from '../assets/troll-face.png'
+
 function Header(){
     return(
         <div className="header-container">
-            <p>Elem 1</p>
-            <p>Elem 2</p>
-            <p>Elem 3</p>
+            <div className="header-title-container">
+                <img src={trollFace} alt="" className='header-logo' />
+                <h1 className="header-title">Meme generator</h1>
+            </div>
+
         </div>
     )
 }
